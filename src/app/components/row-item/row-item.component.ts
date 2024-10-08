7d034d495a6cb98cf59d19c8444d9a754aa2fd37
Pablo Andres Chacon <pabloandres120297@gmail.com>
2024-10-08 16:10:57 -0500

    creando los componentes de la app

---
+++ src/app/components/row-item/row-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './row-item.component.html',
+  styles: ``
+})
+export class RowItemComponent {
+
+}
