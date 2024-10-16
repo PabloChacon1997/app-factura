403392c512b3a2ff54889a16a4ed8a14a8e265aa
Pablo Andres Chacon <pabloandres120297@gmail.com>
2024-10-16 17:21:04 -0500

    Añadiendo formulario y validaciones

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -6,11 +6,13 @@ import { CompanyViewComponent } from '../company-view/company-view.component';
 import { ClientViewComponent } from '../client-view/client-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
 import { TotalComponent } from '../total/total.component';
+import { FormItemComponent } from '../form-item/form-item.component';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [InvoiceViewComponent, CompanyViewComponent, ClientViewComponent, ListItemsComponent, TotalComponent],
+  imports: [InvoiceViewComponent, CompanyViewComponent, ClientViewComponent, ListItemsComponent, TotalComponent, FormItemComponent],
   templateUrl: './invoice.component.html',
   styles: ``
 })
@@ -19,5 +21,13 @@ export class InvoiceComponent implements OnInit{
   constructor(private service: InvoiceService) {}
   ngOnInit(): void {
     this.invoice = this.service.getInvoice();
+  }
+
+  removeItem(id: number){
+    this.invoice = this.service.remove(id);
+  }
+
+  addItem(item: Item) {
+    this.invoice = this.service.save(item);
   }
 }
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,18 @@ export class InvoiceService {
   constructor() { }
 
   getInvoice(): Invoice {
+    const total = this.calculateTotal()
+    return {...this.invoice, total: total};
+  }
+
+  remove(id: number): Invoice {
+    this.invoice.items = this.invoice.items.filter(item => item.id !== id);
+    const total = this.calculateTotal()
+    return {...this.invoice, total: total};
+  }
+
+  save(item: Item): Invoice {
+    this.invoice.items = [...this.invoice.items, item]
     const total = this.calculateTotal()
     return {...this.invoice, total: total};
   }
